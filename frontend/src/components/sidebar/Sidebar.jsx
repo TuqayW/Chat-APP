0bff35f036bc16384d1a635e6b8811999f9941e2
TuqayW <101931875+TuqayW@users.noreply.github.com>
2024-03-24 17:17:41 +0400

    Frontend finished without functions

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import Conversations from "./Conversations";
+import SearchInput from "./SearchInput";
+import LogoutButton from "./LogoutButton"
+import MessageContainer from "../messages/MessageContainer";
+
+const Sidebar = () => {
+	return (
+		<div className='border-r border-slate-500 p-4 flex flex-col'>
+			<SearchInput />
+			<div className='divider px-3'></div>
+			<Conversations/>
+			<LogoutButton/>
+		</div>
+	);
+};
+export default Sidebar; 
